74ba7231f48e2b7cda1ea9200b6879b0167f0f31
Valeria <fedorova.valeria@accenture.com>
2020-01-20 13:44:52 +0300

    Added AddCard component

--- client/src/components/AddCard.vue
+++ client/src/components/AddCard.vue
@@ -1,7 +1,12 @@
 <template>
   <form @submit.prevent="onSubmit">
-    <input v-model="question" type="text" />
-    <button type="submit" >Add new</button>
+    <div class="left-col">
+      <b-form-input v-model="question" placeholder="Enter card's question"></b-form-input>
+      <b-form-input v-model="answer" placeholder="Enter card's answer"></b-form-input>
+    </div>
+    <div class="right-col">
+      <b-button type="submit" variant="primary">+</b-button>
+    </div>
   </form>
 </template>
 
@@ -9,16 +14,37 @@
 export default {
   data() {
     return {
-      question: ""
-    }
+      question: "",
+      answer: ""
+    };
   },
   methods: {
     onSubmit() {
-      if(this.question !== "") {
-        this.$emit("add-card", this.question);
+      if (this.question !== "") {
+        this.$emit("add-card", {
+          question: this.question,
+          answer: this.answer
+        });
         this.question = "";
+        this.answer = "";
       }
     }
   }
-}
+};
 </script>
+
+<style>
+.left-col {
+  width: 90%;
+  float: left;
+}
+.right-col {
+  width: 10%;
+  float: right;
+}
+.right-col button {
+  font-size: 32px;
+  width: 100%;
+  height: 13vh;
+}
+</style>
--- client/src/components/Category.vue
+++ client/src/components/Category.vue
@@ -1,11 +1,27 @@
 <template>
   <div>
-    <h2>{{currCategoryName}}</h2>
-    <router-link to="/">Return</router-link>
-    <AddCard @add-card="addCard" />
-    <Box v-bind:cards="cardsLevel2" />
-    <hr />
-    <Box v-bind:cards="cardsLevel3" />
+    <b-row align-h="center">
+      <b-col lg="6">
+        <h2>{{currCategoryName}}</h2>
+        <router-link to="/">Return</router-link>
+      </b-col>
+    </b-row>
+    <b-row align-h="center">
+      <b-col lg="6">
+        <AddCard @add-card="addCard" />
+      </b-col>
+    </b-row>
+    <b-row align-h="center">
+      <b-col lg="3">
+        <Box v-bind:cards="cardsLevel1" />
+      </b-col>
+      <b-col lg="3">
+        <Box v-bind:cards="cardsLevel2" />
+      </b-col>
+      <b-col lg="3">
+        <Box v-bind:cards="cardsLevel3" />
+      </b-col>
+    </b-row>
   </div>
 </template>
 
@@ -40,21 +56,24 @@ export default {
     };
   },
   computed: {
+    cardsLevel1() {
+      return this.cards.filter(item => item.level == 1);
+    },
     cardsLevel2() {
-      return this.cards.filter(item => item.level == 2)
+      return this.cards.filter(item => item.level == 2);
     },
     cardsLevel3() {
-      return this.cards.filter(item => item.level == 3)
+      return this.cards.filter(item => item.level == 3);
     }
   },
   methods: {
-    addCard(question) {
+    addCard(cardObj) {
       const lastEl = this.cards[this.cards.length - 1];
       const lastIdx = lastEl ? lastEl.id : 0;
       this.cards.push({
         id: lastIdx + 1,
-        question: question,
-        answer: "",
+        question: cardObj.question,
+        answer: cardObj.answer,
         level: 3
       });
     }
